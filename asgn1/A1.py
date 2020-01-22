80ecdd42f122e0323b69d4d6b4ebe3f07cacee08
mavHo <homaverick@gmail.com>
2020-01-22 15:08:08 -0800

    unk fix

--- asgn1/A1.py
+++ asgn1/A1.py
@@ -1,3 +1,4 @@
+import math
 def main():
     # stores a mapping of tokens already seen. generates count.
     vocab = {}
@@ -21,10 +22,19 @@ def initialize(vocab):
 
     ### prepend unks to file the name
     replace_unknowns('A1-Data/1b_benchmark.train.tokens', 'A1-Data/1b_benchmark_unks.train.tokens', vocab)
-    vocab['<unk>'] += replace_unknowns('A1-Data/1b_benchmark.dev.tokens', 'A1-Data/1b_benchmark_unks.dev.tokens',vocab)
+    vocab['<unk>'] += replace_unknowns('A1-Data/1b_benchmark.dev.tokens', 'A1-Data/1b_benchmark_unks.dev.tokens', vocab)
     vocab['<unk>'] += replace_unknowns('A1-Data/1b_benchmark.test.tokens', 'A1-Data/1b_benchmark_unks.test.tokens', vocab)
 
 
+### Given the probabilities of the n-grams
+### calculate the perplexity of the sentence?
+def cal_perplexity(**kwargs):
+    fileset = ['A1-Data/1b_benchmark_unks.train.tokens','A1-Data/1b_benchmark_unks.dev.tokens','A1-Data/1b_benchmark_unks.test.tokens']
+    L = 23
+    #for file in fileset:
+    #    for line in  file_generator(file):
+    #        for token in line.split():
+                ### calculate the perpleity
 
 ### Replace words that don't appear in training data.
 def replace_unknowns(in_file, outfile, vocab):
@@ -36,9 +46,14 @@ def replace_unknowns(in_file, outfile, vocab):
                 if token not in vocab:
                     count += 1
                     #replace replaces all tokens 
-                    line = line.replace(token, '<unk>')
+                    line = line.replace(' ' + token + ' ', ' <unk> ', )
             outfile.write(line.encode('utf-8'))
     return count
+
+def file_generator(file):
+    with open(file = file, mode = 'rb') as _f:
+        for line in _f:
+            yield line.decode('utf-8')
 
 def ngram_generator(file):
     ### read in byte
